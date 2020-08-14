d6a62626eb2323034158de21567d3df303229d4c
DorimeImo <55598184+DorimeImo@users.noreply.github.com>
2020-08-14 17:00:04 +0200

    Initial commit

---
+++ src/test/java/com/example/springbootproject/MockMvc.java
@@ -0,0 +1,32 @@
+package com.example.springbootproject;
+
+
+import com.example.springbootproject.controller.DesignTacoController;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.core.StringContains.containsString;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest
+class WebLayerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private DesignTacoController controller;
+
+    @Test
+    void shouldReturnDefaultMessage() throws Exception {
+        this.mockMvc.perform(get("/")).andDo(print()).andExpect(status().isOk())
+                .andExpect(content().string(containsString("Shop Homepage - Start Bootstrap Template")));
+    }
+
+
+}
